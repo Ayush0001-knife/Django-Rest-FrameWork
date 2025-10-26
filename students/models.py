f2ea15243aee970641244b855c85fbeb7d87fbdc
Ayush <ayushgautam647gmail.com@Ayushs-Beast.local>
2025-10-26 20:48:51 +0530

    first commit

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+      student_id = models.IntegerField(unique=True) 
+      name = models.CharField(max_length=100)
+      age = models.IntegerField()
+      branch = models.CharField(max_length=100)
+
+
+      def __str__(self):
+            return self.name
