3c9b677d069842a2b182380e82cfde5f2edce740
Ayush Gautam <ayushgautam647gmail.com@Ayushs-Beast.local>
2025-11-01 08:53:54 +0530

    New Commit

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
       path('students/',views.studentView),
       path('students/<int:id>/',views.studentDetailView),
+
+      path('employees/',views.Employees.as_view()),
 ] 
--- api/views.py
+++ api/views.py
@@ -5,6 +5,9 @@ from .serializers import StudentSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from employee.models import Employee
+from .serializers import EmployeeSerializer
 
 # Create your views here. 
 @api_view(['GET','POST'])
@@ -43,4 +46,13 @@ def studentDetailView(request, id):
       elif request.method == 'DELETE':
             student.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+      
+
+
+class Employees(APIView):
+      def get(self,request):
+            employees = Employee.objects.all()
+            serializer = EmployeeSerializer(employees,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+
             
