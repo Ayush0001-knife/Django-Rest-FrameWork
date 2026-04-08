88ea717fac660bc0a00ac1e455d4c862b36456e3
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-08 21:35:10 +0530

    git push

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from students.models import Student
 from .serializers import StudentSerializer
@@ -156,6 +156,9 @@ class AgentViewSet(viewsets.ViewSet):
                   return Response(serializer.data,status=status.HTTP_201_CREATED)
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+      def retrieve(self,request,pk=None):
+           agent = get_object_or_404(Agent,pk=pk)
+           serializer=AgentSerializer(agent)
+           return Response(serializer.data,status=status.HTTP_200_OK)
 
- 
 
