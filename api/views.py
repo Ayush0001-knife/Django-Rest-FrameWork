425ad87323e35daf151e64f1491a83bb7c5458be
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-10 06:34:15 +0530

    New Commit

--- api/views.py
+++ api/views.py
@@ -142,38 +142,42 @@ class BotDetail(generics.RetrieveAPIView,generics.DestroyAPIView,generics.Update
 
 
 # Viewset Based using ----- >    viewsets.ViewSet
-class AgentViewSet(viewsets.ViewSet):
+# class AgentViewSet(viewsets.ViewSet):
       
-      def list(self,request):
-            queryset=Agent.objects.all()
-            serializer=AgentSerializer(queryset,many=True)
-            return Response(serializer.data,status=status.HTTP_200_OK)
+#       def list(self,request):
+#             queryset=Agent.objects.all()
+#             serializer=AgentSerializer(queryset,many=True)
+#             return Response(serializer.data,status=status.HTTP_200_OK)
 
-      def create(self,request):
-            serializer=AgentSerializer(data=request.data)
-            if(serializer.is_valid()):
-                  serializer.save()
-                  return Response(serializer.data,status=status.HTTP_201_CREATED)
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#       def create(self,request):
+#             serializer=AgentSerializer(data=request.data)
+#             if(serializer.is_valid()):
+#                   serializer.save()
+#                   return Response(serializer.data,status=status.HTTP_201_CREATED)
+#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
-      def retrieve(self,request,pk=None):
-           agent = get_object_or_404(Agent,pk=pk)
-           serializer=AgentSerializer(agent)
-           return Response(serializer.data,status=status.HTTP_200_OK)
+#       def retrieve(self,request,pk=None):
+#            agent = get_object_or_404(Agent,pk=pk)
+#            serializer=AgentSerializer(agent)
+#            return Response(serializer.data,status=status.HTTP_200_OK)
 
-      def update(self,request,pk=None):
-            agent=get_object_or_404(Agent,pk=pk)
-            serializer=AgentSerializer(agent,data=request.data)
-            if(serializer.is_valid()):
-                  serializer.save()
-                  return Response(serializer.data,status=status.HTTP_200_OK)
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#       def update(self,request,pk=None):
+#             agent=get_object_or_404(Agent,pk=pk)
+#             serializer=AgentSerializer(agent,data=request.data)
+#             if(serializer.is_valid()):
+#                   serializer.save()
+#                   return Response(serializer.data,status=status.HTTP_200_OK)
+#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-      def delete(self,request,pk=None):
-            agent=get_object_or_404(Agent,pk=pk)
-            agent.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+#       def delete(self,request,pk=None):
+#             agent=get_object_or_404(Agent,pk=pk)
+#             agent.delete()
+#             return Response(status=status.HTTP_204_NO_CONTENT)
             
 
+# Viewset Based using ----- >    viewsets.ModelViewSet
+class AgentViewSet(viewsets.ModelViewSet):
+      queryset=Agent.objects.all()
+      serializer_class = AgentSerializer  
 
