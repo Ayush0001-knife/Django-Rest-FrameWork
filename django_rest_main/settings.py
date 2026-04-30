df99e95e987479b70ddccf8d0413835fcad92767
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-01 02:15:14 +0530

    New Commit

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK={
-    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',       # This is for thr Page Number Pagination
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',       # This is for Limit Offset Pagination
     'PAGE_SIZE':2,
 }
