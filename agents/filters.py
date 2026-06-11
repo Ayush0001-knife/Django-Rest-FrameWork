fa5853c2a5829a58fddc66a7117f12f78a0ec476
Ayush Gautam <ayushstudy647@gmail.com>
2026-06-11 21:10:44 +0530

    New Commit

--- agents/filters.py
+++ agents/filters.py
@@ -5,8 +5,20 @@ from .models import Agent
 class AgentFilter(django_filters.FilterSet):
       designation=django_filters.CharFilter(field_name='designation',lookup_expr='iexact')
       agent_name=django_filters.CharFilter(field_name='agent_name',lookup_expr='icontains')
-      agent_id=django_filters.RangeFilter(field_name='agent_id')
+      # agent_id=django_filters.RangeFilter(field_name='agent_id')
+      id_min=django_filters.CharFilter(method="filter_by_id_range",label="From Agent ID")
+      id_max=django_filters.CharFilter(method="filter_by_id_range",label="To Agent ID")
 
       class Meta:
             model=Agent
-            fields=['designation','agent_name','agent_id']
+            # fields=['designation','agent_name','agent_id']
+            fields=['designation','agent_name','id_min','id_max']
+
+
+      def filter_by_id_range(self,queryset,name,value):
+            if (name=='id_min'):
+                  return queryset.filter(agent_id__gte=value)
+            elif (name=='id_max'):
+                  return queryset.filter(agent_id__lte=value)
+            return queryset      
+
